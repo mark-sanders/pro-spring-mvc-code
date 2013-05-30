e05944c00fed7b287c67f22a958d49bc78d552ca
Mark Sanders <mark.a.sanders@capgemini.com>
2013-05-30 15:58:33 +0100

    Added selectDeliveryOptions and place order at end of flow

--- chapter10-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/controller/OrderController.java
+++ chapter10-bookstore/src/main/java/com/apress/prospringmvc/bookstore/web/controller/OrderController.java
@@ -1,16 +1,22 @@
 package com.apress.prospringmvc.bookstore.web.controller;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.apress.prospringmvc.bookstore.domain.Account;
 import com.apress.prospringmvc.bookstore.domain.Book;
 import com.apress.prospringmvc.bookstore.domain.Category;
+import com.apress.prospringmvc.bookstore.domain.Order;
+import com.apress.prospringmvc.bookstore.domain.support.OrderBuilder;
 import com.apress.prospringmvc.bookstore.service.BookstoreService;
 import com.apress.prospringmvc.bookstore.service.CategoryService;
 
@@ -24,7 +30,7 @@ public class OrderController {
 	@Autowired
 	private CategoryService categoryService;
 	
-	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy");
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
 	public OrderForm initializeForm() {
 		OrderForm orderForm = new OrderForm();
@@ -60,5 +66,22 @@ public class OrderController {
 	public void addBook(OrderForm orderForm) {
 		Book book = bookstoreService.findBook(orderForm.getBookId());
 		orderForm.addBook(book, orderForm.getQuantity());
+	}
+	
+	public void placeOrder(final OrderForm orderForm, final Account account) throws ParseException {
+		OrderBuilder orderBuilder = new OrderBuilder() {
+			{
+				deliveryDate(simpleDateFormat.parse(orderForm.getDeliveryDate()));
+				account(account);
+				orderDate(simpleDateFormat.parse(orderForm.getDeliveryDate()));
+			}
+		};
+		
+		for (Entry<Book, Integer> selectedBooks : orderForm.getSelectedBooks().entrySet()) {
+			orderBuilder.addBook(selectedBooks.getKey(), selectedBooks.getValue());
+		}
+		
+		Order order = bookstoreService.store(orderBuilder.build(true));
+		System.out.println(order);
 	}
 }
